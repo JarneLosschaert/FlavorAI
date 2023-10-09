a4116af9145a3c2047cc76f32c677950acfb0106
Lars Meeuws <lars.meeuws@student.howest.be>
2023-10-10 00:06:48 +0200

    Added scanner button

--- lib/ScannerScreen.dart
+++ lib/ScannerScreen.dart
@@ -41,15 +41,45 @@ class _ScannerScreenState extends State<ScannerScreen> {
 
     return Container(
       constraints: const BoxConstraints.expand(),
-      child: FutureBuilder<void>(
-        future: _initializeControllerFuture,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            return CameraPreview(_controller);
-          } else {
-            return const Center(child: CircularProgressIndicator());  // todo: optimize this, maybe use a static controller so the camera doesn't have to load in every time
-          }
-        },
+      child: Stack(
+        fit: StackFit.expand,
+        children: [
+          FutureBuilder<void>(
+            future: _initializeControllerFuture,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                return CameraPreview(_controller);
+              } else {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+            },
+          ),
+          Positioned(
+            bottom: 16.0, // Adjust the bottom position as needed
+            left: 0,
+            right: 0,
+            child: Center(
+                child: IconButton(
+              onPressed: () async {
+                try {
+                  await _initializeControllerFuture;
+                  final image = await _controller.takePicture();
+                } catch (e) {
+                  debugPrint(e.toString());
+                }
+              },
+              icon: Icon(
+                Icons.lens_outlined,
+                size: 96,
+                color: Colors.white.withOpacity(0.8),
+              ),
+              padding: const EdgeInsets.all(12), // Adjust the padding as needed
+              tooltip: 'Start scanning', // Add a tooltip if desired
+            )),
+          ),
+        ],
       ),
     );
   }
